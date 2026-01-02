f6663beef8ea52bfeaaea987c2d6317214667008
Kauã Rodrigues <kauarodrigues.fn@gmail.com>
2026-01-02 20:55:34 -0300

    refactor: atualizar URL e baseUrl para o novo domínio do projeto

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,11 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://docs.proextend.com.br',
+  url: 'https://ldasistemas.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/proextend-integration-docs/',
+
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
