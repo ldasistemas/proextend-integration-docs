9729c29de756c83a68b320ce37f9c44e7d60104e
Kauã Rodrigues <kauarodrigues.fn@gmail.com>
2026-01-03 10:18:20 -0300

    refactor: atualizar cores do ProExtend no CSS

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -110,7 +110,7 @@ const config = {
       colorMode: {
         defaultMode: 'light',
         disableSwitch: false,
-        respectPrefersColorScheme: true,
+        respectPrefersColorScheme: false,
       },
     }),
 };
